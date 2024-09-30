e7a24d1e86994d3327480329c7fac56fe23bfdf4
Kariuki_Ken <irungukenneth433@gmail.com>
2024-09-30 18:51:10 +0300

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,32 @@
 import { Button } from "@/components/ui/button"
+import PatientForm from "@/components/ui/forms/PatientForm"
 import Image from 'next/image'
+import Link from "next/link"
 
 export default function Home() {
   return (
     <div className="flex h-screen max-h-screen">
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[496px]">
-          <Image/>
+          <Image
+            src="/assets/icons/logo-full.svg"
+            alt="Logo"
+            width={1000}
+            height={1000}
+            className="mb-12 h-10 w-fit"
+          />
+
+          <PatientForm/>
+
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="justify-items-end text-dark-600 xl:text-left">
+              © 2024 CarePulse
+            </p>
+            <Link href="/?admin=true">
+              Admin
+            </Link>
+          </div>
+
         </div>
       </section>
     </div>
