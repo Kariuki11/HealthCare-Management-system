4b8df50a81aea74025790dfd4558e833f9af62bb
Kariuki_Ken <irungukenneth433@gmail.com>
2024-10-02 15:48:28 +0300

    changes

--- components/CustomFormField.tsx
+++ components/CustomFormField.tsx
@@ -6,36 +6,107 @@ import {
     FormItem,
     FormLabel,
     FormMessage,
-  } from "@/components/ui/form"
-  import { Input } from "@/components/ui/input"
-  import { Control } from "react-hook-form"
-  import { FormFieldType } from "./forms/PatientForm"
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Control } from "react-hook-form";
+import { FormFieldType } from "./forms/PatientForm";
+import { Button } from "./ui/button";
 
-  interface CustomProps {
-    control: Control<any>,
-    fieldType: FormFieldType
-  }
-
-const CustomFormField = ({ control, fieldType, name }: CustomProps) => {
-  return (
-    <><FormField
-          control={control}
-          name="username"
-          render={({ field }) => (
-              <FormItem>
-                  <FormLabel>Username</FormLabel>
-                  <FormControl>
-                      <Input placeholder="shadcn" {...field} />
-                  </FormControl>
-                  <FormDescription>
-                      This is your public display name.
-                  </FormDescription>
-                  <FormMessage />
-              </FormItem>
-          )} /><Button type="submit">Submit</Button></>
-</form>
-</Form>
-  )
+interface CustomProps {
+    control: Control<any>;
+    fieldType: FormFieldType;
+    name: string; // Include the name prop in CustomProps
 }
 
-export default CustomFormField
+const CustomFormField = ({ control, fieldType, name }: CustomProps) => {
+    return (
+        <>
+            <FormField
+                control={control}
+                name={name} // Use the dynamic name prop
+                render={({ field }) => (
+                    <FormItem>
+                        <FormLabel>Username</FormLabel>
+                        <FormControl>
+                            <Input placeholder="shadcn" {...field} />
+                        </FormControl>
+                        <FormDescription>
+                            This is your public display name.
+                        </FormDescription>
+                        <FormMessage />
+                    </FormItem>
+                )}
+            />
+            <Button type="submit">Submit</Button>
+        </>
+    );
+};
+
+export default CustomFormField;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 'use client'
+// import {
+//     FormControl,
+//     FormDescription,
+//     FormField,
+//     FormItem,
+//     FormLabel,
+//     FormMessage,
+//   } from "@/components/ui/form"
+//   import { Input } from "@/components/ui/input"
+//   import { Control } from "react-hook-form"
+//   import { FormFieldType } from "./forms/PatientForm"
+// import { Button } from "./ui/button"
+
+//   interface CustomProps {
+//     control: Control<any>,
+//     fieldType: FormFieldType
+//   }
+
+// const CustomFormField = ({ control, fieldType, name }: CustomProps) => {
+//   return (
+//     <><FormField
+//           control={control}
+//           name="username"
+//           render={({ field }) => (
+//               <FormItem>
+//                   <FormLabel>Username</FormLabel>
+//                   <FormControl>
+//                       <Input placeholder="shadcn" {...field} />
+//                   </FormControl>
+//                   <FormDescription>
+//                       This is your public display name.
+//                   </FormDescription>
+//                   <FormMessage />
+//               </FormItem>
+//           )} 
+//           />
+//           <Button type="submit">Submit</Button>
+//     </>
+// </form>
+// </Form>
+//   )
+// }
+
+// export default CustomFormField
