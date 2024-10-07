ec1e3db95b7946e44513f268320167d40b22bfbc
Kariuki_Ken <irungukenneth433@gmail.com>
2024-10-07 15:34:02 +0300

    "Added Appwrite import and implemented createUser function with error handling for existing users"

--- Lib/actions/patient.actions.ts
+++ Lib/actions/patient.actions.ts
@@ -1,11 +1,22 @@
+import { Query } from "node-appwrite"
+import { users } from "../appwrite.config"
+
 export const createUser = async(user: CreateUserParams) => {
     try {
-
-    } catch (error) {
+        const newUser = await users.create(
+            ID.unique(),
+            user.email,
+            user.phone,
+            undefined, 
+            user.name
+        )
+    } catch (error: any) {
         if(error && error?.code === 409){
-            const existingUser = await users.list([
+            const documents = await users.list([
                 Query.equal('email', [user.email])
             ])
+
+            return documents?.users[0]
         }
     }
 }
