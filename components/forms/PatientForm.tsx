7b51c6180b58e8a8b437ab616da3cc81f5335c26
Kariuki_Ken <irungukenneth433@gmail.com>
2024-10-07 16:18:01 +0300

    Implement PatientForm component with form validation and user creation functionality

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -1,3 +1,120 @@
+'use client'
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import { Button } from "@/components/ui/button"
+import { Form } from "@/components/ui/form"
+import CustomFormField from "../CustomFormField"
+import SubmitButton from "../SubmitButton"
+import { useState } from "react"
+import { UserFormValidation } from "@/Lib/validation"
+import { useRouter } from "next/navigation"
+import { createUser } from "@/Lib/actions/patient.actions"
+
+export enum FormFieldType {
+  INPUT = 'input',
+  TEXTAREA = 'textarea',
+  PHONE_INPUT = 'phoneInput',
+  CHECKBOX = 'chexkbox',
+  DATE_PICKER = 'datepicker',
+  SELECT = 'select',
+  SKELETON = 'skeleton',
+}
+
+const PatientForm = () => {
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const form = useForm<z.infer<typeof UserFormValidation>>({
+    resolver: zodResolver(UserFormValidation),
+    defaultValues: {
+      name: "",
+      email: "",
+      phone: "",
+    },
+  });
+
+  // Define a submit handler.
+  async function onSubmit({ name, email, phone }: z.infer<typeof UserFormValidation>) {
+    setIsLoading(true); // Start loading when the form is submitted
+
+    try {
+      const userData = { name, email, phone };
+
+      const user = await createUser(userData);
+
+      if (user) {
+        router.push(`/patients/${user.$id}/register`);
+      }
+    } catch (error) {
+      console.error(error);
+    } finally {
+      // Ensure loading stops after either success or error
+      setIsLoading(false);
+    }
+  }
+
+  return (
+    <div>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 flex-1">
+          <section className="mb-12 space-y-4">
+            <h1 className="header">Hi, There👋</h1>
+            <p className="text-dark-700">Schedule Your First Appointment.</p>
+          </section>
+
+          <CustomFormField
+            fieldType={FormFieldType.INPUT}
+            control={form.control}
+            name="name"
+            label="Full Name"
+            placeholder="Enter your full name"
+            iconSrc="/assets/icons/user.svg"
+            iconAlt="user"
+          />
+
+          <CustomFormField
+            fieldType={FormFieldType.INPUT}
+            control={form.control}
+            name="email"
+            label="Email"
+            placeholder="Enter your email address"
+            iconSrc="/assets/icons/email.svg"
+            iconAlt="email"
+          />
+
+          <CustomFormField
+            fieldType={FormFieldType.PHONE_INPUT}
+            control={form.control}
+            name="phone"
+            label="Phone Number"
+            placeholder="Please Enter Your Phone Number"
+            iconSrc="/assets/icons/phone.svg"
+            iconAlt="phone"
+          />
+
+          <SubmitButton isLoading={isLoading}>Get Started</SubmitButton>
+
+        </form>
+      </Form>
+    </div>
+  );
+}
+
+export default PatientForm;
+
+
+
+
+
+
+
+
+
+
+
+
 // "use client"
 
 // import { zodResolver } from "@hookform/resolvers/zod"
